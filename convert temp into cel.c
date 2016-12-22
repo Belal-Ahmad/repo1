9a05f6cf12e30d354035e48dd641807f8414998b
Belal-Ahmad <mdbelal2786@gmail.com>
2016-12-22 19:04:31 +0530

    Add files via upload

---
+++ convert temp into cel.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+        {
+         float c, f;
+         printf(" enter temp in cel");
+         scanf("%f",&c);
+         f= c*9/5+32;
+         printf("\n fahrenheit=%f", f);
+         getch();
+         }
